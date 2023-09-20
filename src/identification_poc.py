0dec75661b02f626c223980e7659006cec6586ac
r-wata <109280369+watanabe-baketsu@users.noreply.github.com>
2023-09-21 00:55:32 +0900

    :+1: Update for torch and Add bigbird

--- src/identification_poc.py
+++ src/identification_poc.py
@@ -1,16 +1,16 @@
 from datasets import load_from_disk, Dataset
-from transformers import AutoProcessor, MarkupLMForQuestionAnswering
+from transformers import AutoProcessor, MarkupLMForQuestionAnswering, BigBirdTokenizer, BigBirdForQuestionAnswering
 from sentence_transformers import SentenceTransformer, util
 import torch
 
 
-def inference_brand(html_string: str) -> str:
+def inference_brand_markuplm(html_string: str) -> str:
     processor = AutoProcessor.from_pretrained("microsoft/markuplm-base-finetuned-websrc")
-    model = MarkupLMForQuestionAnswering.from_pretrained("microsoft/markuplm-base-finetuned-websrc")
+    model = MarkupLMForQuestionAnswering.from_pretrained("microsoft/markuplm-base-finetuned-websrc").to("cuda")
 
     question = "What is the brand name of the website?"
 
-    encoding = processor(html_string, questions=question, return_tensors="pt", truncation=True)
+    encoding = processor(html_string, questions=question, return_tensors="pt", truncation=True).to("cuda")
 
     with torch.no_grad():
         outputs = model(**encoding)
@@ -24,9 +24,46 @@ def inference_brand(html_string: str) -> str:
     return answer
 
 
+# for bigbird
+def get_best_valid_start_end_idx(start_scores, end_scores, top_k=1, max_size=100):
+    best_start_scores, best_start_idx = torch.topk(start_scores, top_k)
+    best_end_scores, best_end_idx = torch.topk(end_scores, top_k)
+
+    widths = best_end_idx[:, None] - best_start_idx[None, :]
+    mask = torch.logical_or(widths < 0, widths > max_size)
+    scores = (best_end_scores[:, None] + best_start_scores[None, :]) - (1e8 * mask)
+    best_score = torch.argmax(scores).item()
+
+    return best_start_idx[best_score % top_k], best_end_idx[best_score // top_k]
+
+
+def inference_brand_bigbird(text: str) -> str:
+    model_name = "google/bigbird-base-trivia-itc"
+    model = BigBirdForQuestionAnswering.from_pretrained(model_name, attention_type="original_full").to("cuda")
+    tokenizer = BigBirdTokenizer.from_pretrained(model_name)
+
+    question = "What is the brand name or title of the website?"
+
+    encoding = tokenizer(question, text, return_tensors='pt', truncation=True).to("cuda")
+    input_ids = encoding.input_ids.to("cuda")
+
+    with torch.no_grad():
+        start_scores, end_scores = model(input_ids=input_ids).to_tuple()
+
+    start_score, end_score = get_best_valid_start_end_idx(start_scores, end_scores, top_k=1, max_size=100)
+
+    # Let's convert the input ids back to actual tokens
+    all_tokens = tokenizer.convert_ids_to_tokens(encoding["input_ids"][0].tolist())
+    answer_tokens = all_tokens[start_score: end_score + 1]
+
+    output = tokenizer.decode(tokenizer.convert_tokens_to_ids(answer_tokens))
+
+    return output
+
+
 def get_similar_brand(inference_brand: str, bland_list: list) -> str:
     # calculate similarity between two brand strings
-    model = SentenceTransformer('all-MiniLM-L6-v2')
+    model = SentenceTransformer('all-MiniLM-L6-v2').to("cuda")
 
     query_embedding = model.encode(inference_brand)
     passage_embedding = model.encode(brand_list)
@@ -38,7 +75,7 @@ def get_similar_brand(inference_brand: str, bland_list: list) -> str:
 
 if __name__ == "__main__":
     # load dataset
-    dataset = load_from_disk("D:/datasets/phishing_identification/phish", keep_in_memory=True)
+    dataset = load_from_disk("D:/datasets/phishing_identification/phish-html-pickup-en", keep_in_memory=True)
     # generate target brand list
     phish = Dataset.from_list(dataset["phish"])
     brand_list = list(set(phish["brand"]))
@@ -48,7 +85,8 @@ if __name__ == "__main__":
     correct_ans = 0
     for data in poc_dataset:
         html_string = data["html"]
-        model_inference = inference_brand(html_string)
+        model_inference = inference_brand_markuplm(html_string)
+
         identified_brand = get_similar_brand(model_inference, brand_list)
         print(f"model inference : {model_inference} / "
               f"identified brand : {identified_brand} / "
@@ -56,5 +94,7 @@ if __name__ == "__main__":
 
         if identified_brand == data["brand"]:
             correct_ans += 1
+        else:
+            print(html_string)
 
     print(f"accuracy : {correct_ans / 100}")
--- src/prepare_dataset.py
+++ src/prepare_dataset.py
@@ -6,6 +6,11 @@ from typing import Literal
 
 from bs4 import BeautifulSoup
 from datasets import Dataset
+from langdetect import detect
+
+
+def replace_multiple_newlines(text):
+    return re.sub(r'\n+', '\n', text)
 
 
 class DatasetGenerator:
@@ -39,19 +44,20 @@ class DatasetGenerator:
                 return False
         return False
 
-    def _shorten_html(self, html_text: str) -> str:
+    @staticmethod
+    def _get_only_text(html_text: str) -> str:
         soup = BeautifulSoup(html_text, 'html.parser')
-        allowed_tags = [
-            'head', 'title', 'meta', 'body', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-            'p', 'strong', 'a', 'img', 'hr', 'table', 'tbody', 'tr', 'th', 'td',
-            'ol', 'ul', 'li', 'ruby', 'label'
-        ]
+        return soup.get_text()
 
-        for tag in soup.find_all(True):
-            if tag.name not in allowed_tags:
-                if not tag.find_all(True, recursive=False):
-                    tag.decompose()
+    @staticmethod
+    def _is_english(text: str) -> bool:
+        lang = detect(text)
+        if lang == "en":
+            return True
+        else:
+            return False
 
+    def _remove_base64(self, soup: BeautifulSoup) -> str:
         # Remove img tags with base64 encoded src attributes
         for img_tag in soup.find_all('img', src=True):
             src_value = img_tag['src']
@@ -66,7 +72,43 @@ class DatasetGenerator:
 
         return text
 
-    def generate_dataset(self) -> Dataset:
+    def _shorten_html(self, html_text: str) -> str:
+        soup = BeautifulSoup(html_text, 'html.parser')
+        allowed_tags = [
+            'head', 'title', 'body', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+            'p', 'strong', 'a', 'img', 'hr', 'table', 'tbody', 'tr', 'th', 'td',
+            'ol', 'ul', 'li', 'ruby', 'label'
+        ]
+
+        for tag in soup.find_all(True):
+            if tag.name not in allowed_tags:
+                if not tag.find_all(True, recursive=False):
+                    tag.decompose()
+
+        text = self._remove_base64(soup)
+
+        return text
+
+    def _shorten_by_text_html(self, html_text: str) -> str:
+        soup = BeautifulSoup(html_text, 'html.parser')
+        allowed_tags = [
+            'head', 'title', 'body', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+            'p', 'strong', 'a', 'img', 'hr', 'table', 'tbody', 'tr', 'th', 'td',
+            'ol', 'ul', 'li', 'ruby', 'label'
+        ]
+
+        for tag in soup.find_all(True):
+            if tag.name not in allowed_tags:
+                if not tag.find_all(True, recursive=False):
+                    tag.decompose()
+            if not tag.get_text(strip=True):
+                tag.decompose()
+
+        text = self._remove_base64(soup)
+
+        return text
+
+    def generate_shortened_html_dataset(self) -> Dataset:
         html_json_pairs = self._read_html_json_pairs()
         dataset = []
         for pair in html_json_pairs:
@@ -77,33 +119,90 @@ class DatasetGenerator:
                     html = f.read()
                 with open(info_path, "r", encoding="utf-8", errors="ignore") as f:
                     info = f.read()
-                info = ast.literal_eval(info)
-                data = {
-                    "html": self._shorten_html(html),
-                    "brand": info.get("brand"),
-                    "url": info.get("url"),
-                    "host": info.get("host"),
-                    "label": self.label,
-                }
-                dataset.append(data)
+                text = replace_multiple_newlines(self._get_only_text(html))
+                if self._is_english(html):  # onlu english html
+                    info = ast.literal_eval(info)
+                    html = self._shorten_html(html)
+                    data = {
+                        "html": f"<p>{text}</p>{html}",
+                        "brand": info.get("brand"),
+                        "url": info.get("url"),
+                        "host": info.get("host"),
+                        "label": self.label,
+                    }
+                    dataset.append(data)
             except Exception as e:
                 print(e)
         dataset = Dataset.from_dict({self.label: Dataset.from_list(dataset)})
+
+        dataset.save_to_disk("D:/datasets/phishing_identification/phish-html-en")
+
+        return dataset
+
+    def generate_pickup_html_dataset(self) -> Dataset:
+        html_json_pairs = self._read_html_json_pairs()
+        dataset = []
+        for pair in html_json_pairs:
+            html_path = pair["html_path"]
+            info_path = pair["info_path"]
+            try:
+                with open(html_path, "r", encoding="utf-8", errors="ignore") as f:
+                    html = f.read()
+                with open(info_path, "r", encoding="utf-8", errors="ignore") as f:
+                    info = f.read()
+                if self._is_english(html):  # only english html
+                    info = ast.literal_eval(info)
+                    data = {
+                        "html": self._shorten_by_text_html(html),
+                        "brand": info.get("brand"),
+                        "url": info.get("url"),
+                        "host": info.get("host"),
+                        "label": self.label,
+                    }
+                    dataset.append(data)
+            except Exception as e:
+                print(e)
+        dataset = Dataset.from_dict({self.label: Dataset.from_list(dataset)})
+
+        dataset.save_to_disk("D:/datasets/phishing_identification/phish-html-pickup-en")
+
+        return dataset
+
+    def generate_text_only_dataset(self) -> Dataset:
+        html_json_pairs = self._read_html_json_pairs()
+        dataset = []
+        for pair in html_json_pairs:
+            html_path = pair["html_path"]
+            info_path = pair["info_path"]
+            try:
+                with open(html_path, "r", encoding="utf-8", errors="ignore") as f:
+                    html = f.read()
+                with open(info_path, "r", encoding="utf-8", errors="ignore") as f:
+                    info = f.read()
+                text = self._get_only_text(html)
+                if self._is_english(text) and text != "":  # onlu english html
+                    info = ast.literal_eval(info)
+                    data = {
+                        "text": replace_multiple_newlines(text),
+                        "brand": info.get("brand"),
+                        "url": info.get("url"),
+                        "host": info.get("host"),
+                        "label": self.label,
+                    }
+                    dataset.append(data)
+            except Exception as e:
+                print(e)
+        dataset = Dataset.from_dict({self.label: Dataset.from_list(dataset)})
+
+        dataset.save_to_disk("D:/datasets/phishing_identification/phish-text-en")
 
         return dataset
 
 
 if __name__ == "__main__":
-    # benign_base_path = "D:/datasets/phishing_identification/benign_sample_30k"
-    # benign_label = "benign"
-    # dataset_generator = DatasetGenerator(benign_base_path, benign_label)
-    # benign_dataset = dataset_generator.generate_dataset()
-    # benign_dataset.save_to_disk("D:/datasets/phishing_identification/benign")
-    # print(benign_dataset.num_rows)
 
     phish_base_path = "D:/datasets/phishing_identification/phish_sample_30k"
     phish_label = "phish"
     dataset_generator = DatasetGenerator(phish_base_path, phish_label)
-    phish_dataset = dataset_generator.generate_dataset()
-    phish_dataset.save_to_disk("D:/datasets/phishing_identification/phish")
+    phish_dataset = dataset_generator.generate_pickup_html_dataset()
     print(phish_dataset.num_rows)
