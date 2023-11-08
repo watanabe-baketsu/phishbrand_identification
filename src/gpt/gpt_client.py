8f4cb322b323a09405a3c68b31f24e20e0d39e80
r-wata <109280369+watanabe-baketsu@users.noreply.github.com>
2023-11-09 01:24:31 +0900

    :recycle: refactoring

--- src/gpt/gpt_client.py
+++ src/gpt/gpt_client.py
@@ -1,11 +1,13 @@
 import os
+import re
 import time
 from os.path import join, dirname
 
 import pandas as pd
 from openai import OpenAI, APIError
 from dotenv import load_dotenv
-from datasets import load_from_disk
+from datasets import load_from_disk, Dataset
+from sentence_transformers import SentenceTransformer, util
 
 from src.gpt.prompt import system_prompt
 
@@ -18,6 +20,15 @@ load_dotenv(dotenv_path)
 class GPTClient:
     def __init__(self):
         self.client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+        self.pattern = r'<brand>(.*?)</brand>'
+
+    def _parse_response(self, contents):
+        inference = re.search(self.pattern, contents, re.DOTALL)
+        if inference is None:
+            return contents
+        else:
+            inference_brand = inference.group(1).strip()
+            return inference_brand
 
     def _request_gpt(self, html_code: str, model_name: str = "gpt-3.5-turbo-1106"):
         response = self.client.chat.completions.create(
@@ -29,30 +40,63 @@ class GPTClient:
             temperature=1.0,
             max_tokens=256,
         )
+        print(response)
         return response
 
     def request_manager(self, html_code: str, model_name: str = "gpt-3.5-turbo-1106") -> str:
         try:
             response = self._request_gpt(html_code, model_name)
-            inference_brand = response.choices[0].message.content.strip()
+            contents = response.choices[0].message.content.strip()
+            inference_brand = self._parse_response(contents)
             return inference_brand
         except APIError as api_error:
-            if api_error.code == 429:
-                print("Too many requests, sleep 60 seconds...")
-                time.sleep(60)
-                print("Retry request...")
-                response = self._request_gpt(html_code, model_name)
-                inference_brand = response.choices[0].message.content.strip()
-                return inference_brand
-            elif api_error.code == 400:
-                print("The request is too large")
-                html_code = html_code[:4*10000]
-                print("Retry request...")
-                response = self._request_gpt(html_code, model_name)
-                inference_brand = response.choices[0].message.content.strip()
-                return inference_brand
+            local_e = str(api_error)
+            if str(api_error).startswith("Error code: 429"):
+                while local_e.startswith("Error code: 429"):
+                    try:
+                        print("Too many requests, sleep 100 seconds...")
+                        time.sleep(100)
+                        print("Retry request...")
+                        response = self._request_gpt(html_code, model_name)
+                        contents = response.choices[0].message.content.strip()
+                        inference_brand = self._parse_response(contents)
+                        return inference_brand
+                    except APIError as err:
+                        local_e = str(err)
+                        time.sleep(20)
+            elif str(api_error).startswith("Error code: 400"):
+                while local_e.startswith("Error code: 400"):
+                    print("The request is too large")
+                    html_code = html_code[:int(len(html_code)*0.9)]
+                    print("Retry request...")
+                    try:
+                        response = self._request_gpt(html_code, model_name)
+                        contents = response.choices[0].message.content.strip()
+                        inference_brand = self._parse_response(contents)
+                        return inference_brand
+                    except APIError as err:
+                        local_e = str(err)
+                        time.sleep(20)
+            else:
+                raise api_error
         except Exception as error:
+            print(error)
             raise error
+
+
+def get_similar_brand(batch):
+    identified_brands = []
+    similarity = []
+    for inference in batch["inference"]:
+        query_embedding = st_model.encode(inference)
+        sim = util.dot_score(query_embedding, passage_embedding).max()
+        similarity.append(sim)
+        if sim < 0.5:
+            identified_brands.append("other")
+        else:
+            identified_brands.append(brand_list[util.dot_score(query_embedding, passage_embedding).argmax()])
+
+    return {"identified": identified_brands, "similarity": similarity}
 
 
 def load_checkpoint_df(checkpoint_path: str) -> pd.DataFrame:
@@ -67,10 +111,14 @@ if __name__ == "__main__":
     model = "gpt-3.5-turbo-1106"  # "gpt-4-1106-preview"
     gpt_client = GPTClient()
 
+    st_model = SentenceTransformer('all-MiniLM-L6-v2')
+
     validation_length = 4000
     # load dataset
     base_path = "D:/datasets/phishing_identification"
     dataset = load_from_disk(f"{base_path}/phish-html-en-qa").select(range(10000, 10000 + validation_length))
+    brand_list = list(set(dataset["title"]))
+    passage_embedding = st_model.encode(brand_list)
 
     # for testing
     # brand = request_gpt(dataset[0]["context"], model)
@@ -81,7 +129,9 @@ if __name__ == "__main__":
     if "inference" not in checkpoint_df.columns:
         checkpoint_df["inference"] = ""
     # select samples that "inference" column is empty
-    targets = checkpoint_df[checkpoint_df["inference"] == ""]
+    targets_null = checkpoint_df[checkpoint_df["inference"].isnull()]
+    targets_empty = checkpoint_df[checkpoint_df["inference"] == ""]
+    targets = pd.concat([targets_null, targets_empty], ignore_index=True)
     count = 0
     for _, sample in targets.iterrows():
         try:
@@ -89,11 +139,22 @@ if __name__ == "__main__":
             checkpoint_df.loc[checkpoint_df["id"] == sample["id"], "inference"] = inference
         except Exception as e:
             print(e)
-            checkpoint_df.to_csv(f"{base_path}/{model}-result.csv", index=False)
+            checkpoint_df.to_csv(f"{base_path}/gpt_results/{model}-result.csv", index=False)
 
-        time.sleep(5)
+        time.sleep(20)
         count += 1
         print(f"\r{count} / {len(targets)}", end="")
 
     checkpoint_df.to_csv(f"{base_path}/gpt_results/{model}-result.csv", index=False)
-    print(f"done : {len(checkpoint_df[checkpoint_df['inference'] != ''])}")
+    print(f"\ndone : {len(checkpoint_df[checkpoint_df['inference'] != ''])}")
+
+    dataset = Dataset.from_pandas(checkpoint_df)
+    dataset = dataset.map(get_similar_brand, batched=True, batch_size=20)
+    dataset.save_to_disk(f"{base_path}/gpt_results/{model}-result")
+
+    acc = 0
+    for data in dataset:
+        if data["identified"] == data["title"]:
+            acc += 1
+    print(f"accuracy : {acc / len(dataset)}")
+
--- src/gpt/prompt.py
+++ src/gpt/prompt.py
@@ -2,4 +2,5 @@ system_prompt = ("You are an excellent phishing site analyst."
                  "Analyze the provided HTML code for indicators of branding, "
                  "ifiable information that could suggest the target of the phishing attempt. "
                  "Output the name of the brand that is likely being impersonated."
-                 "The response format is following: \n'Amazon Inc.'")
+                 "You should respond only the brand name and respond in the following format: "
+                 "\n<brand>Amazon Inc.</brand>")
