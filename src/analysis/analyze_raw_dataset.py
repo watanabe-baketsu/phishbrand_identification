3e19c2f76084d3cebece64f092a3fcf2ad09a0c6
watanabe-baketsu <ryusei.watanabe@fuji.waseda.jp>
2024-01-07 20:00:19 +0900

    Add analysis of file lengths and language distribution
    This commit adds code to analyze the lengths of files in the dataset and
    plot their distribution. It also includes code to count the occurrence
    of different languages in the dataset and plot their distribution. These
    changes will provide valuable insights into the dataset.

--- src/analysis/analyze_raw_dataset.py
+++ src/analysis/analyze_raw_dataset.py
@@ -1,8 +1,13 @@
+import ast
+import json
 import chardet
+import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from langdetect import detect
+from collections import Counter
+from langdetect import detect, LangDetectException
 import glob
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor
 
 
 class RawDatasetAnalysis:
@@ -20,8 +25,7 @@ class RawDatasetAnalysis:
         file_lengths = []
 
         for file in files:
-            encoding = self.find_encoding(file)
-            with open(file, 'r', encoding=encoding, errors="ignore") as f:
+            with open(file, 'r', encoding="utf-8", errors="ignore") as f:
                 content = f.read()
                 if len(content) > 0:
                     file_lengths.append(len(content))
@@ -30,33 +34,118 @@ class RawDatasetAnalysis:
         print('Max:', max(file_lengths))
         print('Min:', min(file_lengths))
         print('Avg:', sum(file_lengths) / len(file_lengths))
-
+        print('Median:', np.median(file_lengths))
+        print('Std:', np.std(file_lengths))
+        print('Q1:', np.quantile(file_lengths, 0.25))
+        print('Q2:', np.quantile(file_lengths, 0.5))
+        print('Q3:', np.quantile(file_lengths, 0.75))
         # plot distribution
-        sns.displot(file_lengths, kde=True)
-        plt.xlabel('File lengths')
-        plt.ylabel('Count')
-        plt.show()
+        # bar plot
+        range_size = 20000
+        counts = Counter((x // range_size) * range_size for x in file_lengths)
+
+        sorted_counts = sorted(counts.items())
+        ranges = [str(x[0]) + "-" + str(x[0] + range_size) for x in sorted_counts]
+        counts = [x[1] for x in sorted_counts]
+
+        fig, ax1 = plt.subplots()  # グラフのサイズを調整
+        ax1.bar(ranges, counts, color='b', alpha=0.6, label='Counts') 
+        ax1.set_ylabel('Counts', color='b')
+        ax1.tick_params('y', colors='b')
+        ax1.set_xticks(range(len(ranges)))
+        ax1.set_xticklabels(ranges, rotation=-90, ha='left', fontsize=7)
+
+        # 累積グラフ（線グラフ）
+        sum_counts = []
+        sum_count = 0
+        for count in counts:
+            sum_count += count
+            sum_counts.append(sum_count)
+        # convert list to pandas series
+        sum_counts = pd.Series(sum_counts)
+
+        ax2 = ax1.twinx()
+        ax2.plot(ranges, sum_counts, color='r', marker='o', label='Sum')
+        ax2.set_ylabel('Sum', color='r')
+        ax2.tick_params('y', colors='r')
+        ax2.set_ylim(bottom=0)  # 折れ線グラフのメモリを0スタートにする
+
+        plt.tight_layout()
+        plt.savefig('file_lengths_distribution.pdf', bbox_inches='tight')
 
     def lang_count_and_stats(self):
         files = glob.glob(self.path + '/**/html.txt', recursive=True)
         lang_counts = {}
 
         for file in files:
-            encoding = self.find_encoding(file)
-            with open(file, 'r', encoding=encoding) as f:
+            with open(file, 'r', encoding="utf-8", errors="ignore") as f:
                 content = f.read()
-                lang = detect(content)
-                lang_counts[lang] = lang_counts.get(lang, 0) + 1
+                if len(content) >= 20:
+                    try:
+                        lang = detect(content)
+                        lang_counts[lang] = lang_counts.get(lang, 0) + 1
+                    except LangDetectException as e:
+                        print(f"Language detection failed for file {file}: {e}")
+                else:
+                    print(f"Skipping file {file} due to insufficient text length.")
 
         print(lang_counts)
 
         # plot
         keys = list(lang_counts.keys())
         values = list(lang_counts.values())
-        plt.bar(keys, values)
+        bars = plt.bar(keys, values)
         plt.xlabel('Languages')
         plt.ylabel('File count')
-        plt.show()
+        # 各棒グラフの上にサンプル数を表示
+        for bar in bars:
+            height = bar.get_height()
+            plt.text(bar.get_x() + bar.get_width() / 2, height,
+                     str(int(height)), ha='center', va='bottom', fontsize=8)
+        plt.tight_layout()
+        plt.savefig('language_distribution.pdf')
+
+    def brand_count_and_stats(self):
+        files = glob.glob(self.path + '/**/info.txt', recursive=True)
+        brand_counts = {}
+
+        for file in files:
+            with open(file, 'r', encoding="utf-8", errors="ignore") as f:
+                try:
+                    info = f.read()
+                    info = info.replace("'", '"')
+                    info = ast.literal_eval(info)
+                    brand = info.get('brand', 'unknown')
+                    brand_counts[brand] = brand_counts.get(brand, 0) + 1
+                except Exception as e:
+                    print(f"Error while reading file {file}: {e}")
+
+        # サンプル数の多い順にブランドを並べ替え
+        sorted_brand_counts = sorted(brand_counts.items(), key=lambda x: x[1], reverse=True)
+        keys = [item[0] for item in sorted_brand_counts]
+        values = [item[1] for item in sorted_brand_counts]
+
+        # plot
+        fig, ax1 = plt.subplots()
+        ax1.bar(keys, values, color='b', label='Count')
+        ax1.set_ylabel('File count')
+        ax1.tick_params('y', colors='b')
+        ax1.set_xticks([])  # X軸のブランド名を表示しない
+
+        # 累積グラフ（線グラフ）
+        sum_counts = [sum(values[:i+1]) for i in range(len(values))]  # 累積サンプル数を計算
+        ax2 = ax1.twinx()
+        ax2.plot(keys, sum_counts, color='r', marker='o', label='Sum')
+        ax2.set_ylabel('Sum', color='r')
+        ax2.tick_params('y', colors='r')
+        ax2.set_ylim(bottom=0)
+
+        plt.tight_layout()
+        plt.savefig('brand_distribution.pdf')
+
+        print(f"Total number of brands: {len(keys)}")
+
+
 
 
 def analyze_char_stats(path: str):
@@ -68,12 +157,21 @@ def analyze_lang_stats(path: str):
     analysis = RawDatasetAnalysis(path)
     analysis.lang_count_and_stats()
 
+def analyze_brand_stats(path: str):
+    analysis = RawDatasetAnalysis(path)
+    analysis.brand_count_and_stats()
+
 
 def main():
-    path = 'D:/datasets/phishpedia_vanilla/phish_sample_30k'
-    analyze_char_stats(path)
-    analyze_lang_stats(path)
-
+    path = '/mnt/d/datasets/phishpedia_vanilla/phish_sample_30k'
+    # ProcessPoolExecutorを使用して並列処理を行う
+    with ProcessPoolExecutor() as executor:
+        # 各関数を並列に実行するためのFutureオブジェクトを取得
+        executor.submit(analyze_char_stats, path)
+        #executor.submit(analyze_lang_stats, path)
+        #executor.submit(analyze_brand_stats, path)
+        pass
+        
 
 if __name__ == '__main__':
     main()
